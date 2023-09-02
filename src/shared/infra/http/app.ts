9d5dd90d2f35bf50c8e4d2c2ea84ee927e205829
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 02:34:44 -0300

    add dependency to treat async errors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import 'express-async-errors';
 import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import { errors } from 'celebrate';
@@ -11,25 +12,25 @@ import router from './route';
 
 const app = express();
 
-app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-      return res.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-  
-    console.error(err);
-  
-    return res.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
-    });
-});  
-
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 app.use(errors());
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+  });
+});  
 
 export default app;
