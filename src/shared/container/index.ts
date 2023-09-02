51ee6080636b217c91f9fd1ca4213c03d622ab5a
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 14:07:28 -0300

    register task repository on tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import BoardImplementation from '@modules/board/infra/database/implementation/BoardImplementation';
 import { IBoardRepository } from '@modules/board/infra/database/repository/IBoardRepository';
+import TaskImplementation from '@modules/task/infra/database/implementation/TaskImplementation';
+import { ITaskRepository } from '@modules/task/infra/database/repository/ITaskRepository';
 
 container.registerSingleton<IBoardRepository>(
     'BoardRepository',
     BoardImplementation,
+);
+
+container.registerSingleton<ITaskRepository>(
+    'TaskRepository',
+    TaskImplementation,
 );
