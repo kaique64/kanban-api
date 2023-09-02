2ca63dab8d9b93915634b7460ff0c0f1087e6b7b
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 02:14:27 -0300

    add celebrate middleware for validation

--- src/modules/board/infra/http/route/board.routes.ts
+++ src/modules/board/infra/http/route/board.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { celebrate, Joi, Segments } from 'celebrate';
 import BoardController from "../controllers/BoardController";
 
 const boardRoute = Router();
 const boardController = new BoardController();
 
-boardRoute.post('/', boardController.create);
+boardRoute.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+        },
+    }),
+    boardController.create
+);
 
 export default boardRoute;
