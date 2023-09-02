4e9e5d05253809e53050884e84ef860b60b5b168
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 01:40:08 -0300

    implement board service

---
+++ src/modules/board/service/implementation/BoardService.ts
@@ -0,0 +1,30 @@
+import { IBoardDTO } from "@modules/board/dtos/IBoardDTO";
+import { IBoardResponseDTO } from "@modules/board/dtos/IBoardResponseDTO";
+import { IBoardUpdateDTO } from "@modules/board/dtos/IBoardUpdateDTO";
+import { IBoardService } from "../interface/IBoardService";
+import { inject, injectable } from "tsyringe";
+import { IBoardRepository } from "@modules/board/infra/database/repository/IBoardRepository";
+
+@injectable()
+class BoardService implements IBoardService {
+    
+    constructor(
+        @inject('BoardRepository')
+        private boardRepository: IBoardRepository,
+    ) {}
+
+    public async create(boardDTO: IBoardDTO): Promise<IBoardResponseDTO> {
+        return await this.boardRepository.create(boardDTO);
+    }
+    
+    public async update(boardDTO: IBoardUpdateDTO): Promise<IBoardResponseDTO | undefined> {
+        return await this.boardRepository.create(boardDTO);
+    }
+
+    public async delete(id: number): Promise<boolean> {
+        return await this.boardRepository.delete(id);
+    }
+
+}
+
+export default BoardService
