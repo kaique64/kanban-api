0ab259a2691cadf681e7281e418821f481e2da13
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 00:16:37 -0300

    create class to response errors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,25 @@
 import 'dotenv/config';
-import express from 'express';
 import '../database'
+import express, { Request, Response, NextFunction } from 'express';
+import AppError from '../errors/AppError';
 
 const app = express();
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return res.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+  
+    console.error(err);
+  
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});  
 
 app.use(express.json());
 
