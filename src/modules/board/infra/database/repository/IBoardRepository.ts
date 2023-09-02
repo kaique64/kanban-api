3eeee0d565d4176df65007336e74113f2dbcf6fa
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 01:13:19 -0300

    create board repository

---
+++ src/modules/board/infra/database/repository/IBoardRepository.ts
@@ -0,0 +1,13 @@
+import { IBoardDTO } from "@modules/board/dtos/IBoardDTO";
+import { IBoardResponseDTO } from "@modules/board/dtos/IBoardResponseDTO";
+import { IBoardUpdateDTO } from "@modules/board/dtos/IBoardUpdateDTO";
+
+export interface IBoardRepository {
+
+    create(boardDTO: IBoardDTO): Promise<IBoardResponseDTO>;
+    
+    update(boardDTO: IBoardUpdateDTO): Promise<IBoardResponseDTO>;
+    
+    delete(id: number): Promise<boolean>;
+
+}
