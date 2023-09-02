4fee7aac3f9d2cc60bcf07b8b2ce19da6d40050a
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 02:21:09 -0300

    add update method to controller

--- src/modules/board/infra/http/controllers/BoardController.ts
+++ src/modules/board/infra/http/controllers/BoardController.ts
@@ -3,10 +3,21 @@ import { Request, Response } from "express";
 import BoardService from "@modules/board/service/implementation/BoardService";
 
 class BoardController {
+    private boardService = container.resolve(BoardService);
+
     public async create(req: Request, res: Response): Promise<Response> {
         const { name } = req.body;
-        const boardService = container.resolve(BoardService);
-        const board = await boardService.create({ name });
+
+        const board = await this.boardService.create({ name });
+
+        return res.status(200).json(board);
+    }
+
+    public async update(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        const board = await this.boardService.update({ id: Number(id), name });
 
         return res.status(200).json(board);
     }
